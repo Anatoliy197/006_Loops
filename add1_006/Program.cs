dea62273bbd2a133d8ea73c00083166d7a7a2b97
Anatoliy197 <etc143@yandex.ru>
2021-07-07 10:03:48 +0300

    Добавьте файлы проекта.

---
+++ add1_006/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace add1_006
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x = 0; x < 4; x++)
+            {
+                for(int y = 0; y < 10; y++)
+                {
+                    Console.Write("@");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
